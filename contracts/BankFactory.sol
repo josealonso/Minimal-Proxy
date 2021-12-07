017b955a16ce374ea3c1654e9c89b75175e6e3ca
josealonso <jose.alonso.programmer@gmail.com>
2021-12-07 18:01:32 +0100

    Created the template for a minimal proxy pattern, using hardhat and TypeScript.

---
+++ contracts/BankFactory.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "./Bank.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BankFactory is Ownable {
+    /*Variables*/
+    struct BankTag {
+        address bankAddress;
+    }
+
+    address public bankAddress;
+    BankTag[] private _banks;
+
+    event BankCreated(address newBankAddress, address owner);
+
+    constructor(address _bankAddress) {
+        bankAddress = _bankAddress;
+    }
+
+    function createBank(
+        string memory name,
+        uint256 interestRate,
+        uint256 originationFee,
+        uint256 collateralizationRatio,
+        uint256 liquidationPenalty,
+        uint256 period,
+        address payable oracleAddress
+    ) public {
+        address clone = Clones.clone(bankAddress);
+        Bank(clone).init(
+            msg.sender,
+            name,
+            interestRate,
+            originationFee,
+            collateralizationRatio,
+            liquidationPenalty,
+            period,
+            owner(),
+            oracleAddress
+        );
+        BankTag memory newBankTag = BankTag(clone);
+        _banks.push(newBankTag);
+        emit BankCreated(clone, msg.sender);
+    }
+
+    function getNumberOfBanks() public view returns (uint256) {
+        return _banks.length;
+    }
+
+    function getBankAddressAtIndex(uint256 index)
+        public
+        view
+        returns (address)
+    {
+        BankTag storage bank = _banks[index];
+        return bank.bankAddress;
+    }
+}
